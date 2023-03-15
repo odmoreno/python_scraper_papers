5606106623f91bb4e8b985e6ebdaf260bbae6083
Oscar Moreno <oscar.moreno@cti.espol.edu.ec>
2023-03-15 16:18:58 -0500

    config

--- 12_data_for_vis.py
+++ 12_data_for_vis.py
@@ -14,28 +14,53 @@ class dataVis:
         self.authors_vinci = {}
         self.authors_2009 = {}
         self.authors = {}
+        self.countries = {}
 
     def load_data(self):
         self.institutions = load_generic(self.base + 'insti.json')
         self.papers_upd = load_generic('data/vinci_2009/papers_update.json')
         self.authors_vinci = load_generic('data/jsons/authors.json')
         self.authors_2009 = load_generic('data/vinci_2009/authors.json')
+        self.countries = load_generic('data/jsons/countries.json')
 
     def fill_authors_2009(self):
         papers = {}
+        counter = 1
         for doi, pvinci in self.papers_upd.items():
             authors = pvinci['authors']
             list_ids = []
-
+            list_instis = []
             for author in authors:
                 id = author['id']
                 institution = author['institution']
+
+                for i in institution:
+                    list_instis.append(i['id'])
+
+
                 if id in self.authors_2009:
                     self.authors_2009[id]['institutions'] = institution
+                    if 'papers' not in self.authors_2009[id]:
+                        self.authors_2009[id]['papers'] = []
+                        self.authors_2009[id]['papers'].append(doi)
+                    else:
+                        self.authors_2009[id]['papers'].append(doi)
+                if id in self.authors_vinci:
+                    if 'papers' not in self.authors_vinci[id]:
+                        self.authors_vinci[id]['papers'] = []
+                        self.authors_vinci[id]['papers'].append(doi)
+                    else:
+                        self.authors_vinci[id]['papers'].append(doi)
                 list_ids.append(id)
+
             pvinci['authors'] = list_ids
-            pvinci['id'] = doi
+            pvinci['authors'] = list_instis
+
+            pvinci['doi'] = doi
             pvinci['conference'] = 'vinci'
+
+            pvinci['id'] = counter
+            counter += 1
 
             if 'citations' in pvinci:
                 del pvinci['citations']
@@ -51,13 +76,12 @@ class dataVis:
                 del pvinci['n_reference']
             if 'conference_title' in pvinci:
                 del pvinci['conference_title']
-            if 'doi' in pvinci:
-                del pvinci['doi']
-
-
+            #if 'doi' in pvinci:
+            #    del pvinci['doi']
 
         print('hi')
         save_generic('data/dataforvis/authors_2009.json', self.authors_2009)
+        save_generic('data/dataforvis/authors_vinci.json', self.authors_vinci)
         save_generic('data/dataforvis/nodes.json', self.papers_upd)
 
     def config_vinci_au(self):
@@ -69,25 +93,51 @@ class dataVis:
     def merge_authors(self):
         authors_vinci = {}
         for id, list in self.authors_vinci.items():
+
+            instis = list['institutions']
+            new = []
+            for i in instis:
+                if i['id'] != 'javascript:void(0)':
+                    new.append(i['id'])
+
             data = {
-                'id': list['id'],
+                'doi': list['id'],
                 'name': list['name'],
                 'url': list['url'],
-                'institutions': list['institutions'],
+                'institutions': new,
+                'papers': list['papers'],
+                'n_insti': len(new),
+                'n_paper': len(list['papers']),
             }
             authors_vinci[id] = data
 
         authors_2009 = {}
         for id, list in self.authors_2009.items():
+            print(list)
+            instis = list['institutions']
+            new = []
+
+            print(instis)
+            if list['name'] == 'Kang Zhang':
+                print('hmm')
+
+            for i in instis:
+                if i['id'] != 'javascript:void(0)':
+                    new.append(i['id'])
+
             data = {
-                'id': list['id'],
+                'doi': list['id'],
                 'name': list['name'],
                 'url': '',
-                'institutions': list['institutions'],
+                'institutions': new,
+                'papers': list['papers'],
+                'n_insti': len(new),
+                'n_paper': len(list['papers']),
             }
             authors_2009[id] = data
 
         self.authors = authors_2009 | authors_vinci
+
         save_generic('data/dataforvis/authors.json', self.authors)
 
     def fix_papers(self):
